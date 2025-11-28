c4f62af158dafca978e09552f775f54b8a9be207
DoraChad <176503791+DoraChad@users.noreply.github.com>
2025-11-27 20:17:34 -0800

    Update main.mod.js

--- 1.0.0/main.mod.js
+++ 1.0.0/main.mod.js
@@ -1,16 +1,117 @@
 import { PolyMod, MixinType } from "https://pml.crjakob.com/cb/PolyTrackMods/PolyModLoader/0.5.2/PolyModLoader.js";
 
-class YourMod extends PolyMod {
-    init = (pml) => {
-        // regular init
+const ElementStack = {
+    stack: [],
+    
+    currentAtDepth(depth = null) {
+        if (depth === null) {
+            return this.stack[this.stack.length - 1]?.wrapper || null;
+        };
+
+        for (let i = this.stack.length - 1; i >= 0; i--) {
+            if (this.stack[i].depth <= depth) return this.stack[i].wrapper;
+        }
+        return null;
+    },
+    
+    push(wrapper, depth) {
+        this.stack.push({ wrapper, depth });
+    },
+
+    popToDepth(depth) {
+        while (this.stack.length > 0 && this.stack[this.stack.length - 1].depth > depth) {
+            this.stack.pop();
+        }
+    },
+
+    clear() {
+        this.stack = [];
     }
-    postInit = () => {
-        // post init
+    
+}
+
+class ElementHandler {
+    constructor(element, depth = 0) {
+        this.el = element;
+        this.depth = depth;
     }
-    simInit = () => {
-        // sim init here
-    } 
+
+    _append(element, depthOffset = 1) {
+        const parent = ElementStack.currentAtDepth(this.depth);
+        
+        parent.el.appendChild(element);
+
+        const handler = new ElementHandler(element, this.depth + depthOffset);
+        ElementStack.push(handler, handler.depth);
+
+        return handler;
+    };
+
+    button(text) {
+        const button = document.createElement("button");
+
+        if (text) {
+            const textEl = document.createElement("p");
+            textEl.textContent = text;
+
+            button.appendChild(textEl);
+        }
+
+        return this._append(button);
+    }
+    
+    background(color) {
+        const target = ElementStack.currentAtDepth(this.depth);
+        (target?.el)?.style.background = color;
+        return this;
+    }
 }
 
 
-export let polyMod = new YourMod();
+// PRESETS
+const elementPresets = {
+    button: (parent, text) => {
+        const button = document.createElement("button");
+
+        if (text) {
+            const textEl = document.createTextNode(text);
+
+            button.appendChild(textEl);
+        }
+
+        return parent._append(button);
+    }
+};
+
+
+for (const [name, fn] of Object.entries(elementPresets)) {
+
+    ElementHandler.prototype[name] = function(...args) {
+        return fn(this, ...args);
+    };
+}
+
+const API = {
+    build(root, fn) {
+        const handler = new ElementHandler(root, 0);
+        ElementStack.push(handler, 0);
+        fn();
+        ElementStack.clear();
+    }
+};
+
+for (const [name, fn] of Object.entries(elementPresets)) {
+    API[name] = function(...args) {
+        const parent = ElementStack.currentAtDepth();
+        return fn(parent, ...args);
+    };
+}
+
+class PUI extends PolyMod {
+    init = (pml) => {
+        pml.registerFuncMixin("polyInitFunction", MixinType.HEAD, "window.PUI = ActivePolyModLoader.getMod('pui').API");
+    }
+}
+
+
+export let polyMod = new PUI();
